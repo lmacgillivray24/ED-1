8199b64443d685b401d7183a3b317ddf10b92aa5
lmacgillivray24 <laurmac118@gmail.com>
2024-01-09 08:44:12 -0500

    Gif

--- CreativeCoding/2x2/sketch.js
+++ CreativeCoding/2x2/sketch.js
@@ -1,7 +1,39 @@
 function setup() {
- 
+ createCanvas (720,720);
+  background (100,70,250);
+  fill ('lightblue');
+  strokeWeight (5);
 }
 
 function draw() {
   
+  translate (60,60); //resets orgin point
+ 
+  quad (
+    0,0,
+    300,0,
+    300,300,
+    0,300
+  );
+  
+    quad (
+    300,0,
+    600,0,
+    600,300,
+    300,300
+  );//move 300 pixels in the x
+  
+    quad (
+    300,300,
+    600,300,
+    600,600,
+    300,600
+  );//move 300 pixels in the y
+  
+    quad (
+    0,300,
+    300,300,
+    300,600,
+    0,600
+  );//move 300 pixels in the y
 }
--- CreativeCoding/Square/sketch.js
+++ CreativeCoding/Square/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
- 
+ createCanvas (720,720);
+  background (100,70,250);
+  fill ('lightblue');
+  strokeWeight (5);
 }
 
 function draw() {
   
+  translate (60,60); //resets orgin point
+ 
+  quad (
+    0,0,
+    300,0,
+    300,300,
+    0,300
+  );
 }
