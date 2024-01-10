b77be0a5794a342beae5bf9d1e4376047cfdb524
lmacgillivray24 <laurmac118@gmail.com>
2024-01-10 13:46:33 -0500

    add gif to grid

---
+++ CreativeCoding/FinalBG/sketch.js
@@ -0,0 +1,33 @@
+//adding a GIF to my grid
+
+let gif;
+
+function preload ('assets/GIFTransparent.gif');
+
+
+function setup() {
+ createCanvas (windowWidth,windowHeight);
+  fill ('lightBlue');
+  strokeWeight (1);
+}
+
+function draw() {
+
+  var num = 50;
+  var sideLen = windowWidth/num;
+ 
+    for (var y = 0; y < windowHeight; y = y + sideLen)
+  {
+    
+  for (var x = 0; x < windowWidth; x = x + sideLen){
+ image (gif, x, y, sidLen, sideLen);
+  }
+  
+  function windowResized (){
+  resizeCanvas (windowWidth, windowHeight)
+}  
+}
+
+function windowResized (){
+resizeCanvas (windowWidth, windowHeight)
+}
