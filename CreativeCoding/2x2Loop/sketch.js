81e41f6972539054bdb600b3dfdf09b0730420d8
lmacgillivray24 <laurmac118@gmail.com>
2024-01-10 13:37:08 -0500

    update

--- CreativeCoding/2x2Loop/sketch.js
+++ CreativeCoding/2x2Loop/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
- 
+ createCanvas (720,720);
+  background (100,70,250);
+  fill ('lightblue');
+  strokeWeight (5);
 }
 
 function draw() {
   
+  translate (60,60); //resets orgin point
+ 
+    for (var y = 0; y < 600; y = y + 300)
+  {
+    
+  
+  
+  for (var x = 0; x < 600; x = x + 300)
+  {
+     quad (
+    x,y,
+    x+300,y,
+    x+300,y + 300,
+    x,y + 300
+  );//loop to create a row in the x direction
+  }
+  }
 }
--- CreativeCoding/TuningWithVariables/sketch.js
+++ CreativeCoding/TuningWithVariables/sketch.js
@@ -1,7 +1,33 @@
 function setup() {
- 
+ createCanvas (windowWidth,windowHeight);
+  fill ('lightBlue');
+  strokeWeight (1);
 }
 
 function draw() {
+
+  var num = 100;
+  var sideLen = windowWidth/num;
+ 
+    for (var y = 0; y < windowHeight; y = y + sideLen)
+  {
+    
+  for (var x = 0; x < windowWidth; x = x + sideLen)
+  {
+     quad (
+    x,y,
+    x+sideLen,y,
+    x+sideLen,y + sideLen,
+    x,y + sideLen
+  );//loop to create a row in the x direction
+  }
+  }
   
+  function windowResized (){
+  resizeCanvas (windowWidth, windowHeight)
+}  
+}
+
+function windowResized (){
+resizeCanvas (windowWidth, windowHeight)
 }
