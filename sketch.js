ef1eff53c7b0db437d1540d514ed90eacb617cc2
lmacgillivray24 <laurmac118@gmail.com>
2024-01-24 11:31:35 -0500

    update

---
+++ sketch.js
@@ -0,0 +1,32 @@
+//adding a GIF to my grid
+
+let gif;
+
+function preload (){
+gif = loadImage ('assets/GIFTransparent.gif');
+}
+
+
+function setup() {
+ createCanvas (windowWidth,windowHeight);
+  fill ('lightBlue');
+  strokeWeight (1);
+}
+
+function draw() {
+background ('white');
+  var num = 5;
+  var sideLen = windowWidth/num;
+ 
+    for (var y = 0; y < windowHeight; y = y + sideLen)
+  {
+    
+  for (var x = 0; x < windowWidth; x = x + sideLen){
+ image (gif, x, y, sideLen, sideLen);
+  }
+}  
+}
+
+function windowResized (){
+	resizeCanvas (windowWidth, windowHeight);
+}
