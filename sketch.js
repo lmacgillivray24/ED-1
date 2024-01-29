d7c80eaaf6935651f4ba663eb3f37986fe853b55
lmacgillivray24 <laurmac118@gmail.com>
2024-01-29 09:29:06 -0500

    update

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ background ('white');
     for (var y = 0; y < windowHeight; y = y + sideLen)
   {
     
-  for (var x = 0; x < windowWidth; x = x + sideLen){
+  for (var x = 0; x < 2 * windowWidth; x = x + sideLen){
  image (gif, x, y, sideLen, sideLen);
   }
 }  
